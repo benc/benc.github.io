de838bed3b5a874aac1d903f5abda70dabf04394
Ben Cochez <ben@cochez.info>
2015-06-05 20:48:19 +0200

    sass update

--- tasks/sass.js
+++ tasks/sass.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
+var order = require('gulp-order');
+var debug = require('gulp-debug');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync');
 
@@ -18,6 +20,14 @@ gulp.task('sass', function() {
   };
 
   return gulp.src(['./_sass/**/*.sass', './_sass/**/*.scss'])
+    .pipe(order([
+      '**/_base.scss',
+      '**/_layout.scss',
+      '**/_heavy_industries.sass',
+      '**/*.sass',
+      '**/*.scss'
+    ]))
+    .pipe(debug())
     .pipe(sass(sassOpts).on('error', errorHandler))
     .pipe(gulp.dest('_site/css'))
     .pipe(browserSync.reload({
