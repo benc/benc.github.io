105085aa02bbf10d1156978eee66f19b5d582217
Ben Cochez <ben@cochez.info>
2015-06-12 21:50:45 +0200

    update sass files, lato font

--- tasks/sass.js
+++ tasks/sass.js
@@ -8,30 +8,20 @@ var debug = require('gulp-debug');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync');
 
-var errorHandler = notify.onError(function(error) {
-  gutil.log(error.plugin + ' ERROR ' + error);
-  return error.plugin + ' ' + error.name;
+var errorHandler = notify.onError(function (error) {
+    gutil.log(error.plugin + ' ERROR ' + error);
+    browserSync.notify;
+    return error.plugin + ' ' + error.name;
 });
 
 gulp.task('sass', function() {
   var sassOpts = {
-    errLogToConsole: true,
-    onError: browserSync.notify
+    errLogToConsole: true
   };
 
   return gulp.src(['./_sass/**/*.sass', './_sass/**/*.scss'])
-    .pipe(order([
-      '**/_base.scss',
-      '**/_layout.scss',
-      '**/_heavy_industries.sass',
-      '**/*.sass',
-      '**/*.scss'
-    ]))
     .pipe(debug())
     .pipe(sass(sassOpts).on('error', errorHandler))
-    .pipe(gulp.dest('_site/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    .pipe(gulp.dest('./_site/css'))
     .pipe(gulp.dest('./css'));
 });
